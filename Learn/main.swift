c96caafe5f4209900953d9e4a61471ac95029549
David Pulloquinga <david@pulloquinga.com>
2014-06-08 14:20:00 -0500

    Initial Commit

---
+++ Learn/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Learn
+//
+//  Created by David Pulloquinga on 08/06/14.
+//  Copyright (c) 2014 David Pulloquinga. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
